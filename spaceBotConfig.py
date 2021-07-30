7b7bbbff0dc59029b31a612639ada8e167dc75f5
quackersian <54241767+quackersian@users.noreply.github.com>
2021-07-30 13:35:16 +0100

    Update spaceBotConfig.py

--- spaceBotConfig.py
+++ spaceBotConfig.py
@@ -4,5 +4,5 @@ scriptVersion = "v0.5.0"
 cams = ["f", "n", "r", "m"]
 ownerID = 195617048569708545
 githubLink = "https://github.com/quackersian/space-bot"
-discordServer = "https://discord.gg/XnAGQSbMYM"
+discordServer = "https://discord.gg/9KeQnS94gS"
 topgg = "https://top.gg/bot/849246857309323284"
