c1c1422a447b076d4d01328f3b5cea456f382008
Ian Millard <131303903+Ian-c-m@users.noreply.github.com>
2023-04-20 13:38:10 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import logging, disnake
 
-script_version = "0.7.4"
-script_date = "2022-12-17"
+script_version = "0.7.6"
+script_date = "2023-04-20"
 bot_name = "SpaceBot"
 github_link = "https://github.com/quackersian/space-bot"
 discord_server = "https://discord.gg/x7CyFRA5s6"
@@ -11,6 +11,5 @@ invite_link_short = "https://bit.ly/3Pp0u0n"
 logging_level = logging.INFO
 logging_filemode = "a"
 log_file = "disnake_space_bot.log"
-test_guild_id = 864438904954159104
 needed_intents = disnake.Intents.default()
 status = "with telescopes"
